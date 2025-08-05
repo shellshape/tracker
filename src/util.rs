24cfad7cff74717d2a40b5ce142d124ee58b4140
Ringo Hoffmann <r.hoffmann@b12-gruppe.de>
2025-08-05 11:20:27 +0200

    feat(util): add "yesterday" (`y`) syntax to date parsing

--- src/util.rs
+++ src/util.rs
@@ -69,7 +69,7 @@ impl Entry {
 
         if long {
             if let Some(ref long_message) = self.long {
-                write!(f, " {}", format_long(long_message).italic())?;
+                write!(f, "\n{}", prefix_lines(long_message, "\t").italic())?;
             }
         }
 
@@ -93,12 +93,26 @@ impl Entry {
     }
 }
 
-fn format_long(long: &str) -> String {
+/// Splits the given string by newline character (`\n`) and prepends
+/// the given prefix in front of each line.
+fn prefix_lines(long: &str, prefix: &str) -> String {
     let mut str = String::new();
-    for line in long.split('\n') {
-        str.push_str("\n\t");
+
+    let mut split = long.split('\n');
+
+    let Some(line) = split.next() else {
+        return str;
+    };
+
+    str.push_str(prefix);
+    str.push_str(line);
+
+    for line in split {
+        str.push('\n');
+        str.push_str(prefix);
         str.push_str(line);
     }
+
     str
 }
 
@@ -108,8 +122,28 @@ pub struct Parsable<T: Clone>(pub T);
 impl FromStr for Parsable<NaiveDate> {
     type Err = anyhow::Error;
 
+    /// Parses a given date string as following:
+    ///
+    /// The following table assumes the current date as `2025-07-24` for the
+    /// examples.
+    ///
+    /// | Format | Description | Example | Resulting Date |
+    /// |--------|-------------|---------|----------------|
+    /// | `<yyyy>-<mm>-<dd>` | Concrete date. | `2025-07-24` | `2025-07-24` |
+    /// | `<mm>-<dd>` | Concrete date; year is taken from current date. | `07-24` | `2025-07-24` |
+    /// | `<dd>` | Concrete date; year and month is taken from current date. | `24` | `2025-07-24` |
+    /// | `-<n_days>` | Today minus <n_days>. | `-2` | `2025-07-22` |
+    /// | `y[y...]` | Today minus count of 'y'. | `yyy` | `2025-07-21` |
     fn from_str(date: &str) -> std::result::Result<Self, Self::Err> {
         let today = Local::now().date_naive();
+
+        let y_count = date.chars().take_while(|&c| c == 'y' || c == 'Y').count();
+        if y_count > 0 {
+            if y_count != date.len() {
+                return Err(anyhow::anyhow!("additional characters after 'y'"));
+            }
+            return Ok(Self(today - Duration::days(y_count as i64)));
+        }
 
         if let Some(days_str) = date.strip_prefix('-') {
             let days = days_str.parse()?;
@@ -158,5 +192,74 @@ impl fmt::Display for FormatableEntry<'_, '_> {
         self.entry
             .format(f, self.config, self.long, None::<chrono::TimeDelta>)
             .map_err(|_| fmt::Error)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::util::Parsable;
+    use chrono::{Datelike, Duration, Local, NaiveDate};
+
+    #[test]
+    fn format_long() {
+        assert_eq!("\t", super::prefix_lines("", "\t"));
+        assert_eq!("\t\n\t\n\t\n\t", super::prefix_lines("\n\n\n", "\t"));
+        assert_eq!("\tfoo", super::prefix_lines("foo", "\t"));
+        assert_eq!("\tfoo\n\tbar", super::prefix_lines("foo\nbar", "\t"));
+        assert_eq!("\tfoo\n\tbar\n\t", super::prefix_lines("foo\nbar\n", "\t"));
+    }
+
+    #[test]
+    fn parse_date() {
+        fn get_date<S: AsRef<str>>(date: S) -> NaiveDate {
+            NaiveDate::parse_from_str(date.as_ref(), "%Y-%m-%d").unwrap()
+        }
+
+        fn get_date_parts(date: NaiveDate) -> (u32, u32, u32) {
+            let (_, year) = date.year_ce();
+            let month = date.month0() + 1;
+            let day = date.day0() + 1;
+            (year, month, day)
+        }
+
+        let (year, month, _) = get_date_parts(Local::now().date_naive());
+        assert_eq!(
+            get_date("2025-07-24"),
+            "2025-07-24".parse::<Parsable<NaiveDate>>().unwrap().0
+        );
+        assert_eq!(
+            get_date(format!("{year}-07-24",)),
+            "07-24".parse::<Parsable<NaiveDate>>().unwrap().0
+        );
+        assert_eq!(
+            get_date(format!("{year}-{month}-24",)),
+            "24".parse::<Parsable<NaiveDate>>().unwrap().0
+        );
+
+        assert_eq!(
+            (Local::now() - Duration::days(1)).date_naive(),
+            "-1".parse::<Parsable<NaiveDate>>().unwrap().0
+        );
+        assert_eq!(
+            (Local::now() - Duration::days(2)).date_naive(),
+            "-2".parse::<Parsable<NaiveDate>>().unwrap().0
+        );
+        assert_eq!(
+            (Local::now() - Duration::days(69)).date_naive(),
+            "-69".parse::<Parsable<NaiveDate>>().unwrap().0
+        );
+
+        assert_eq!(
+            (Local::now() - Duration::days(1)).date_naive(),
+            "y".parse::<Parsable<NaiveDate>>().unwrap().0
+        );
+        assert_eq!(
+            (Local::now() - Duration::days(2)).date_naive(),
+            "yY".parse::<Parsable<NaiveDate>>().unwrap().0
+        );
+        assert_eq!(
+            (Local::now() - Duration::days(6)).date_naive(),
+            "YYYYYY".parse::<Parsable<NaiveDate>>().unwrap().0
+        );
     }
 }
